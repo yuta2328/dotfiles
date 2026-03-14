81db9efdb5bee2ed17b75994cd03a8027fd2954d
Yuta Natsui <xiajingyoutai@gmail.com>
2026-03-15 02:38:36 +0900

    delete redundant packages

--- flake.nix
+++ flake.nix
@@ -29,10 +29,18 @@
       };
     };
     homeConfigurations = {
+      mymac = inputs.home-manager.lib.homeManagerConfiguration {
+        pkgs = import inputs.nixpkgs {
+          system = "aarch64-darwin";
+        };
+        modules = [
+          ./home/darwin.nix
+        ];
+      };
       myubuntu = inputs.home-manager.lib.homeManagerConfiguration {
-	      pkgs = import inputs.nixpkgs {
-	        system = "x86_64-linux";
-	      };
+        pkgs = import inputs.nixpkgs {
+          system = "x86_64-linux";
+        };
         modules = [
           ./home/default.nix
         ];
